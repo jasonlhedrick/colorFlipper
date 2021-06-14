6573c82f7c9b7e2cb174cbaa4f298a869f68f0af
Jason Hedrick <jasonleehedrick@gmail.com>
2021-06-14 16:34:20 -0700

    Changed the span id to color-code and the color variable to colorCodeTextContainer to reduce variable name confusion. Switched from using innerHTML to innerText on the colorCodeTextContainer.

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const changeColorBtn = document.getElementById("changeBtn");
-const color = document.querySelector(".color");
+const colorCodeTextContainer = document.querySelector(".color-code");
 
 changeBtn.addEventListener('click', () => {
     const red = randnumber(0, 255);
     const green = randnumber(0, 255);
     const blue = randnumber(0, 255);
     document.body.style.background = `rgb(${red}, ${green}, ${blue})`;
-    color.innerHTML = `rgb(${red}, ${green}, ${blue})`;
+    colorCodeTextContainer.innerText = `rgb(${red}, ${green}, ${blue})`;
 });
 
 function randnumber(min, max) {
